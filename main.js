200ec96612156fc33dd1a0fff6820a61f2f16cf1
ItsRohitt <rhtse564@gmail.com>
2024-10-07 00:58:30 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+onload = () =>{
+        document.body.classList.remove("container");
+};
+
+
+$(document).ready(function () {
+        $('.container').mouseenter(function () {
+            $('.card').stop().animate({
+                top: '-90px'
+            }, 'slow');
+        }).mouseleave(function () {
+            $('.card').stop().animate({
+                top: 0
+            }, 'slow');
+        });
+    });
